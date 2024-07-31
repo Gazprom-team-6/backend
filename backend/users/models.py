2b258b239f95a8558b47b10f17e6c418bb2ed4ea
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-07-31 18:55:11 +0200

    Users: Skills

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,9 @@
-from backend.users.constants import EMPLOYEE_STATUS, JOB_TYPES
-from backend.users.validators import phone_regex, validate_birth_date
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from users.constants import EMPLOYEE_STATUS, JOB_TYPES
+from users.constants import GRADES
+from users.validators import phone_regex, validate_birth_date
 
 
 class GazpromUser(AbstractUser):
@@ -19,38 +21,67 @@ class GazpromUser(AbstractUser):
     employee_avatar = models.ImageField(verbose_name="Аватар")
     employee_telegram = models.CharField(
         verbose_name="Телеграм",
-        max_length=50
+        unique=True,
+        max_length=50,
     )
     employee_telephone = models.CharField(
-        validators=[phone_regex], max_length=20, unique=True,
-        verbose_name="Номер телефона"
+        validators=[phone_regex],
+        max_length=20,
+        unique=True,
+        verbose_name="Номер телефона",
     )
     employee_type_job = models.CharField(
         verbose_name="Тип занятости",
         choices=JOB_TYPES,
-        max_length=50
+        max_length=50,
 
     )
     employee_status = models.CharField(
         verbose_name="Статус",
         choices=EMPLOYEE_STATUS,
-        max_length=50
+        max_length=50,
     )
     employee_location = models.CharField(
         verbose_name="Локация",
-        max_length=300
+        max_length=300,
     )
     employee_grade = models.CharField(
         verbose_name="Грейд",
-        max_length=50
+        choices=GRADES,
+        max_length=50,
     )
     employee_description = models.TextField(verbose_name="Биография")
-    employee_outsource = models.BooleanField(verbose_name="Outsource")
+    is_employee_outsource = models.BooleanField(
+        verbose_name="Outsource",
+        default=False,
+    )
     is_superuser = models.BooleanField(
         default=False,
-        verbose_name="Суперпользователь"
+        verbose_name="Суперпользователь",
     )
+
+    skills = models.ManyToManyField(
+        to="Skill",
+        verbose_name="Навыки"
+    )
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "Пользователи"
+        default_related_name = "users"
+
 
 
 class Skill(models.Model):
     """Навыки сотрудников."""
+
+    name = models.CharField(
+        verbose_name="Название",
+        max_length=150,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = "навык"
+        verbose_name_plural = "Навыки"
+        default_related_name = "skills"
