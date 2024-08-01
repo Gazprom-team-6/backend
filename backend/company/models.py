b77927613171a553ad04cf4343d65171702bc38a
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-08-01 11:55:14 +0200

    AdditionalField, Metric

--- backend/company/models.py
+++ backend/company/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 User = get_user_model()
 
@@ -158,3 +160,51 @@ class Component(models.Model):
         verbose_name = "компонент"
         verbose_name_plural = "Компоненты"
         default_related_name = "component"
+
+    def __str__(self):
+        return self.component_name
+
+
+class AdditionalField(models.Model):
+    """Модель для дополнительных полей."""
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    name = models.CharField(verbose_name="Название", max_length=255)
+    description = models.TextField(verbose_name="Содержание поля")
+
+    class Meta:
+        verbose_name = "компонент"
+        verbose_name_plural = "Компоненты"
+        default_related_name = "component"
+        indexes = [
+            models.Index(fields=['content_type', 'object_id']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Metric(models.Model):
+    """Модель метрик."""
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    name = models.CharField(verbose_name="Название", max_length=255)
+    description = models.TextField(verbose_name="Содержание метрики")
+
+    class Meta:
+        verbose_name = "метрика"
+        verbose_name_plural = "Метрики"
+        default_related_name = "metric"
+        indexes = [
+            models.Index(fields=['content_type', 'object_id']),
+        ]
+
+    def __str__(self):
+        return self.name
+
