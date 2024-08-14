49d8c750df8273f136ab6589b94b15b880d44193
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-08-14 11:35:34 +0200

    Celery: docker, worker, app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
     'DATE_FORMAT': '%d-%m-%Y',
     'DATE_INPUT_FORMATS': ['%d-%m-%Y'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': int(os.getenv('PAGE_SIZE')),
+    'PAGE_SIZE': int(os.getenv('PAGE_SIZE', 5)),
 }
 
 SIMPLE_JWT = {
